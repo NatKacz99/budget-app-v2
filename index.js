f079b3fc4616b96fc4fb03d318e2bf21cb4280f4
NatKacz99 <156061003+NatKacz99@users.noreply.github.com>
2024-09-21 13:51:59 +0200

    added final version

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-document.getElementById("zaloguj-sie").addEventListener("click", function () {
+document.getElementById("log-in").addEventListener("click", function () {
   window.open("strona_logowania.html");
 });
 
-document.getElementById("zarejestruj-sie").addEventListener("click", function () {
-  window.open("strona_rejestracji.html", "_blank");
-});
-
-document.getElementsByClassName("strona-glowna")[0].addEventListener("click", function () {
-  window.open("index.html");
+document.getElementById("sign-up").addEventListener("click", function () {
+  window.open("strona_rejestracji.html");
 });
 
